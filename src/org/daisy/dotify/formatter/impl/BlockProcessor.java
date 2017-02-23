06276224702f7d557f624245c98ddc955cb57a6b
Joel Håkansson <joel.hakansson@mtm.se>
2017-02-23 15:21:56 +0100

    Separates ScenarioData from BlockProcessing

---
+++ src/org/daisy/dotify/formatter/impl/BlockProcessor.java
@@ -0,0 +1,133 @@
+package org.daisy.dotify.formatter.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.daisy.dotify.api.formatter.FormattingTypes.BreakBefore;
+import org.daisy.dotify.api.formatter.FormattingTypes.Keep;
+
+/**
+ * Provides data about a single rendering scenario.
+ * 
+ * @author Joel Håkansson
+ */
+abstract class BlockProcessor {
+	private int keepWithNext = 0;
+	
+	abstract void newRowGroupSequence(VerticalSpacing vs);
+	
+	abstract boolean hasSequence();
+	abstract boolean hasResult();
+	abstract void addRowGroup(RowGroup rg);
+	abstract RowGroup peekResult();
+	
+	BlockProcessor() {
+		this.keepWithNext = 0;
+	}
+
+	BlockProcessor(BlockProcessor template) {
+		this.keepWithNext = template.keepWithNext;
+	}
+
+	private int getKeepWithNext() {
+		return keepWithNext;
+	}
+
+	private void setKeepWithNext(int keepWithNext) {
+		this.keepWithNext = keepWithNext;
+	}
+		
+	void processBlock(LayoutMaster master, Block g, AbstractBlockContentManager bcm) {
+		if (!hasSequence() || ((g.getBreakBeforeType()==BreakBefore.PAGE  || g.getVerticalPosition()!=null) && hasResult())) {
+            newRowGroupSequence(
+                    g.getVerticalPosition()!=null?
+                            new VerticalSpacing(g.getVerticalPosition(), new RowImpl("", bcm.getLeftMarginParent(), bcm.getRightMarginParent()))
+                                    :null
+            );
+			setKeepWithNext(-1);
+		}
+		List<RowGroup> store = new ArrayList<>();
+		List<RowImpl> rl1 = bcm.getCollapsiblePreContentRows();
+		if (!rl1.isEmpty()) {
+			store.add(new RowGroup.Builder(master.getRowSpacing(), rl1).
+									collapsible(true).skippable(false).breakable(false).build());
+		}
+		List<RowImpl> rl2 = bcm.getInnerPreContentRows();
+		if (!rl2.isEmpty()) {
+			store.add(new RowGroup.Builder(master.getRowSpacing(), rl2).
+									collapsible(false).skippable(false).breakable(false).build());
+		}
+		
+		if (bcm.getRowCount()==0) { //TODO: Does this interfere with collapsing margins? 
+			if (!bcm.getGroupAnchors().isEmpty() || !bcm.getGroupMarkers().isEmpty() || !"".equals(g.getIdentifier())
+					|| g.getKeepWithNextSheets()>0 || g.getKeepWithPreviousSheets()>0 ) {
+				RowGroup.Builder rgb = new RowGroup.Builder(master.getRowSpacing(), new ArrayList<RowImpl>());
+				setProperties(rgb, bcm, g);
+				store.add(rgb.build());
+			}
+		}
+
+		int i = 0;
+		List<RowImpl> rl3 = bcm.getPostContentRows();
+		OrphanWidowControl owc = new OrphanWidowControl(g.getRowDataProperties().getOrphans(),
+														g.getRowDataProperties().getWidows(), 
+														bcm.getRowCount());
+		for (RowImpl r : bcm) {
+			i++;
+			r.setAdjustedForMargin(true);
+			if (i==bcm.getRowCount()) {
+				//we're at the last line, this should be kept with the next block's first line
+				setKeepWithNext(g.getKeepWithNext());
+			}
+			RowGroup.Builder rgb = new RowGroup.Builder(master.getRowSpacing()).add(r).
+					collapsible(false).skippable(false).breakable(
+							r.allowsBreakAfter()&&
+							owc.allowsBreakAfter(i-1)&&
+							getKeepWithNext()<=0 &&
+							(Keep.AUTO==g.getKeepType() || i==bcm.getRowCount()) &&
+							(i<bcm.getRowCount() || rl3.isEmpty())
+							);
+			if (i==1) { //First item
+				setProperties(rgb, bcm, g);
+			}
+			store.add(rgb.build());
+			setKeepWithNext(getKeepWithNext()-1);
+		}
+		if (!rl3.isEmpty()) {
+			store.add(new RowGroup.Builder(master.getRowSpacing(), rl3).
+				collapsible(false).skippable(false).breakable(getKeepWithNext()<0).build());
+		}
+		List<RowImpl> rl4 = bcm.getSkippablePostContentRows();
+		if (!rl4.isEmpty()) {
+			store.add(new RowGroup.Builder(master.getRowSpacing(), rl4).
+				collapsible(true).skippable(true).breakable(getKeepWithNext()<0).build());
+		}
+		if (store.isEmpty() && hasSequence()) {
+			RowGroup gx = peekResult();
+			if (gx!=null && gx.getAvoidVolumeBreakAfterPriority()==g.getAvoidVolumeBreakInsidePriority()
+					&&gx.getAvoidVolumeBreakAfterPriority()!=g.getAvoidVolumeBreakAfterPriority()) {
+				gx.setAvoidVolumeBreakAfterPriority(g.getAvoidVolumeBreakAfterPriority());
+			}
+		} else {
+			for (int j=0; j<store.size(); j++) {
+				RowGroup b = store.get(j);
+				if (j==store.size()-1) {
+					b.setAvoidVolumeBreakAfterPriority(g.getAvoidVolumeBreakAfterPriority());
+				} else {
+					b.setAvoidVolumeBreakAfterPriority(g.getAvoidVolumeBreakInsidePriority());
+				}
+				addRowGroup(b);
+			}
+		}
+	}
+	
+	private static void setProperties(RowGroup.Builder rgb, AbstractBlockContentManager bcm, Block g) {
+		if (!"".equals(g.getIdentifier())) { 
+			rgb.identifier(g.getIdentifier());
+		}
+		rgb.markers(bcm.getGroupMarkers());
+		rgb.anchors(bcm.getGroupAnchors());
+		rgb.keepWithNextSheets(g.getKeepWithNextSheets());
+		rgb.keepWithPreviousSheets(g.getKeepWithPreviousSheets());
+	}
+}
--- src/org/daisy/dotify/formatter/impl/RowGroupDataSource.java
+++ src/org/daisy/dotify/formatter/impl/RowGroupDataSource.java
@@ -8,17 +8,66 @@ import org.daisy.dotify.common.split.Supplements;
 class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 	private final LayoutMaster master;
 	private final BlockContext bc;
-	private final ScenarioData data;
+	private final RowGroupData data;
 	private final Supplements<RowGroup> supplements;
 	private final int offset;
 	private final VerticalSpacing vs;
 	private final List<Block> blocks;
 	private int blockIndex;
 	
+	private class RowGroupData extends BlockProcessor {
+		private RowGroupSequence rgs; 
+		
+		RowGroupData() {
+			this.rgs = null;
+		}
+		RowGroupData(RowGroupData template) {
+			this.rgs = template.rgs;
+		}
+
+		@Override
+		void newRowGroupSequence(VerticalSpacing vs) {
+			if (rgs!=null) {
+				throw new IllegalStateException();
+			} else {
+				rgs = new RowGroupSequence(vs);
+			}
+		}
+
+		@Override
+		boolean hasSequence() {
+			return rgs!=null;
+		}
+
+		@Override
+		boolean hasResult() {
+			return hasSequence() && !rgs.getGroup().isEmpty();
+		}
+
+		@Override
+		void addRowGroup(RowGroup rg) {
+			rgs.getGroup().add(rg);
+		}
+
+		@Override
+		RowGroup peekResult() {
+			return rgs.currentGroup();
+		}
+		
+		List<RowGroup> getList() {
+			return rgs.getGroup();
+		}
+		
+		int size() {
+			return rgs==null || rgs.getGroup()==null?0:rgs.getGroup().size();
+		}
+		
+	}
+	
 	RowGroupDataSource(LayoutMaster master, BlockContext bc, RowGroupSequence rgs, Supplements<RowGroup> supplements) {
 		this.master = master;
 		this.bc = bc;
-		this.data = new ScenarioData();
+		this.data = new RowGroupData();
 		this.blocks = rgs.getBlocks();
 		this.offset = 0;
 		this.supplements = supplements;
@@ -26,7 +75,7 @@ class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 		this.blockIndex = 0;
 	}
 	
-	private RowGroupDataSource(LayoutMaster master, BlockContext bc, ScenarioData data, List<Block> blocks, Supplements<RowGroup> supplements, int offset, VerticalSpacing vs, int blockIndex) {
+	private RowGroupDataSource(LayoutMaster master, BlockContext bc, RowGroupData data, List<Block> blocks, Supplements<RowGroup> supplements, int offset, VerticalSpacing vs, int blockIndex) {
 		this.master = master;
 		this.bc = bc;
 		this.data = data;
@@ -53,12 +102,12 @@ class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 
 	@Override
 	public boolean hasElementAt(int index) {
-		return this.data.getSingleGroup().size()>index+offset;
+		return this.data.size()>index+offset;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return this.data.getSingleGroup().size()<=offset && blockIndex>=blocks.size();
+		return this.data.size()<=offset && blockIndex>=blocks.size();
 	}
 
 	@Override
@@ -66,19 +115,19 @@ class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 		if (!ensureBuffer(n)) {
 			throw new IndexOutOfBoundsException("" + n);
 		}
-		return this.data.getSingleGroup().get(offset+n);
+		return this.data.getList().get(offset+n);
 	}
 	
 	public List<RowGroup> head(int n) {
 		if (!ensureBuffer(n-1)) {
 			//throw new IndexOutOfBoundsException();
 		}
-		return this.data.getSingleGroup().subList(offset, offset+n);
+		return this.data.getList().subList(offset, offset+n);
 	}
 	
 	public List<RowGroup> getRemaining() {
 		ensureBuffer(-1);
-		return this.data.getSingleGroup().subList(offset, data.getSingleGroup().size());
+		return this.data.getList().subList(offset, data.size());
 	}
 
 	@Override
@@ -86,14 +135,14 @@ class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 		if (!ensureBuffer(n)) {
 			throw new IndexOutOfBoundsException("" + n);
 		}
-		return new RowGroupDataSource(master, bc, new ScenarioData(data), blocks, supplements, offset+n, vs, blockIndex);
+		return new RowGroupDataSource(master, bc, new RowGroupData(data), blocks, supplements, offset+n, vs, blockIndex);
 	}
 
 	@Override
 	public int getSize(int limit) {
 		if (!ensureBuffer(limit-1))  {
 			//we have buffered all elements
-			return this.data.getSingleGroup().size()-offset;
+			return this.data.size()-offset;
 		} else {
 			return limit;
 		}
@@ -110,7 +159,7 @@ class RowGroupDataSource implements SplitPointDataSource<RowGroup> {
 	 * @return returns true if the index element was available, false otherwise
 	 */
 	private boolean ensureBuffer(int index) {
-		while (index<0 || this.data.getSingleGroup().size()-offset<=index) {
+		while (index<0 || this.data.size()-offset<=index) {
 			if (blockIndex>=blocks.size()) {
 				return false;
 			}
