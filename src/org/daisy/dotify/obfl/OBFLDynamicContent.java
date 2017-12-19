d0ece3521f80bf03d210319cdd8b0aa6f07a6e37
Joel Håkansson <joel.hakansson@mtm.se>
2017-12-19 11:20:24 +0100

    Restores and deprecates the original package

---
+++ src/org/daisy/dotify/obfl/OBFLDynamicContent.java
@@ -0,0 +1,33 @@
+package org.daisy.dotify.obfl;
+
+import org.daisy.dotify.api.formatter.Context;
+import org.daisy.dotify.api.formatter.DynamicContent;
+import org.daisy.dotify.api.obfl.ExpressionFactory;
+import org.daisy.dotify.formatter.impl.obfl.impl.ExpressionTools;
+
+@Deprecated
+public class OBFLDynamicContent extends OBFLExpressionBase implements DynamicContent {
+
+	public OBFLDynamicContent(String exp, ExpressionFactory ef, boolean extended) {
+		super(exp, ef, extended);
+	}
+
+	@Override
+	public String render() {
+		if (exp==null) {
+			return "";
+		} else {
+			return ef.newExpression().evaluate(exp).toString();
+		}
+	}
+
+	@Override
+	public String render(Context context) {
+		if (exp==null) {
+			return "";
+		} else {
+			return ef.newExpression().evaluate(ExpressionTools.resolveVariables(exp, buildArgs(context))).toString();
+		}
+	}
+
+}
---
+++ src/org/daisy/dotify/obfl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * Provides parsing of OBFL-files (Open Braille Formatting Language).
+ * </p>
+ * @author Joel Håkansson
+ */
+@Deprecated
+package org.daisy.dotify.obfl;
