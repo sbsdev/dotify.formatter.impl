7fa17cfa7a064f2135e706a23d35ede68990d565
Joel Håkansson <joel.hakansson@mtm.se>
2018-11-28 14:08:10 +0100

    Re-enable text flow in header/footer test
    This commit is based on a commit by Bert Frees

--- integrationtest/org/daisy/dotify/formatter/test/FlowInHeaderOrFooterTest.java
+++ integrationtest/org/daisy/dotify/formatter/test/FlowInHeaderOrFooterTest.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 
 import org.daisy.dotify.api.engine.LayoutEngineException;
 import org.daisy.dotify.api.writer.PagedMediaWriterConfigurationException;
-import org.junit.Ignore;
 import org.junit.Test;
 
 @SuppressWarnings("javadoc")
 public class FlowInHeaderOrFooterTest extends AbstractFormatterEngineTest {
 	
 	@Test
-	@Ignore("Enable once functionality has been restored.")
 	public void testFlowInFooter() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/flow-in-footer-input.obfl",
 		        "resource-files/flow-in-footer-expected.pef",
@@ -19,7 +17,6 @@ public class FlowInHeaderOrFooterTest extends AbstractFormatterEngineTest {
 	}
 	
 	@Test
-	@Ignore
 	public void testFlowInFooterWithMarkerReference() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/flow-in-footer-with-marker-reference-input.obfl",
 		        "resource-files/flow-in-footer-with-marker-reference-expected.pef",
@@ -27,7 +24,6 @@ public class FlowInHeaderOrFooterTest extends AbstractFormatterEngineTest {
 	}
 	
 	@Test
-	@Ignore("Enable once functionality has been restored.")
 	public void testFlowInHeader() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/flow-in-header-input.obfl",
 		        "resource-files/flow-in-header-expected.pef",
