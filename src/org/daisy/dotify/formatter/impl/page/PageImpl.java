de84853d4e0f554d8011d0de3407858cf84f93d4
Joel Håkansson <joel.hakansson@mtm.se>
2018-12-05 11:26:51 +0100

    Separate bottom page area

--- src/org/daisy/dotify/formatter/impl/page/PageImpl.java
+++ src/org/daisy/dotify/formatter/impl/page/PageImpl.java
@@ -149,8 +149,22 @@ public class PageImpl implements Page {
 			&& !pageArea.isEmpty();
 	}
 	
+	private boolean hasBottomPageArea() {
+		return master.getPageArea()!=null
+			&& master.getPageArea().getAlignment() == PageAreaProperties.Alignment.BOTTOM
+			&& !pageArea.isEmpty();
+	}
+	
 	private void addTopPageArea() {
 		if (hasTopPageArea()) {
+			finalRows.addAll(pageAreaTemplate.getBefore());
+			finalRows.addAll(pageArea);
+			finalRows.addAll(pageAreaTemplate.getAfter());
+		}
+	}
+	
+	private void addBottomPageArea() {
+		if (hasBottomPageArea()) {
 			finalRows.addAll(pageAreaTemplate.getBefore());
 			finalRows.addAll(pageArea);
 			finalRows.addAll(pageAreaTemplate.getAfter());
@@ -186,11 +200,7 @@ public class PageImpl implements Page {
 		            while (Math.ceil(finalRows.getOffsetHeight() + areaSize) < getFlowHeight() + headerHeight) {
 						finalRows.addRow(new RowImpl());
 					}
-					if (master.getPageArea()!=null && master.getPageArea().getAlignment()==PageAreaProperties.Alignment.BOTTOM && !pageArea.isEmpty()) {
-						finalRows.addAll(pageAreaTemplate.getBefore());
-						finalRows.addAll(pageArea);
-						finalRows.addAll(pageAreaTemplate.getAfter());
-					}
+					addBottomPageArea();
 					for (FieldList fields : template.getFooter()) {
 						finalRows.addRow(fieldResolver.renderField(getDetails(), fields, filter));
 					}
